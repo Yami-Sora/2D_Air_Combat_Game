a11a6d46f1e4e7fd0a66fad266d903ee59ff9dd6
Yami-Sora <yamisora002@gmail.com>
2025-08-01 10:31:27 +0700

    Add ShipMovement

---
+++ Assets/_Script/ShipMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    [SerializeField] protected Vector3 worldPosition;
+    [SerializeField] protected float speed = 0.1f;
+
+    private void FixedUpdate()
+    {
+        this.worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.worldPosition.z = 0; 
+        Vector3 newPos = Vector3.Lerp(transform.position, this.worldPosition, this.speed);
+        transform.position = newPos;
+    }
+}
