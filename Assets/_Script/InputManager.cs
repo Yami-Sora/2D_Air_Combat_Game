a2a6031f59f7433b014cd6b7e16cf6b8cac664e4
Yami-Sora <yamisora002@gmail.com>
2025-08-06 21:47:42 +0700

    Update

--- Assets/_Script/InputManager.cs
+++ Assets/_Script/InputManager.cs
@@ -8,11 +8,18 @@ public class InputManager : MonoBehaviour
     [SerializeField] protected Vector3 mouseWorldPos;
     public Vector3 MouseWorldPos { get => mouseWorldPos; }
 
+    [SerializeField] protected float onFiring;
+    public float OnFiring { get => onFiring;}
     private void Awake()
     {
         if(InputManager.instance != null) 
             Debug.LogError("InputManager already exists in the scene. Please ensure there is only one instance.");
         InputManager.instance = this;
+    }
+
+    private void Update()
+    {
+        this.GetMouseDown();
     }
     private void FixedUpdate()
     {
@@ -21,5 +28,10 @@ public class InputManager : MonoBehaviour
     protected virtual void GetMousePos()
     {
         this.mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    protected virtual void GetMouseDown()
+    {
+        this.onFiring = Input.GetAxis("Fire1");
     }
 }
--- Assets/_Script/Ship/ShipShooting.cs
+++ Assets/_Script/Ship/ShipShooting.cs
@@ -3,19 +3,36 @@ using UnityEngine;
 public class ShipShooting : MonoBehaviour
 {
     [SerializeField] protected bool isShooting = false;
+    [SerializeField] protected float shootDelay = 1f; 
+    [SerializeField] protected float shootTimer = 0f;
     [SerializeField] protected Transform bulletPrefab;
 
     private void FixedUpdate()
     {
+        this.checkIsShooting();
         this.Shooting();
     }
 
     protected virtual void Shooting()
     {
         if (!this.isShooting) return;
+
+        if (this.shootTimer < this.shootDelay) 
+        {
+            this.shootTimer += Time.deltaTime;
+            return;
+        }
+
+        this.shootTimer = 0f;
         Vector3 spawnPos = transform.position;
         Quaternion rotation = transform.parent.rotation;
         Instantiate(this.bulletPrefab, spawnPos, rotation);
         Debug.Log("Shooting");
+    }
+
+    protected virtual void checkIsShooting()
+    {
+        if (InputManager.Instance.OnFiring != 0) this.isShooting = true;
+        else this.isShooting = false;
     }
 }
